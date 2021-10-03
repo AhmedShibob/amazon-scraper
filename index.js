b560020b9ce90a1dc44c0644b81d5de088b3eb40
Ahmed Shibob <ashibob.yousef@gmail.com>
2021-10-03 20:46:56 +0200

    iniy

---
+++ index.js
@@ -0,0 +1,86 @@
+const express =  require('express');
+const request = require('request-promise')
+
+
+const app = express();
+
+const  PORT = process.env.PORT || 5000
+
+
+
+const returnScraperApiUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API.');
+})
+
+// GET Product Details 
+
+app.get('/products/:productId', async (req, res) => {
+  const {productId} = req.params;
+  const { api_key } = req.query;
+
+
+  try {
+      const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+      res.json(JSON.parse(response))
+  } catch (error) {
+      
+  }
+})
+
+// GET Product Reviews 
+
+app.get('/products/:productId/reviews', async (req, res) => {
+    const {productId} = req.params;
+    const { api_key } = req.query;
+
+  
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
+        res.json(JSON.parse(response))
+    } catch (error) {
+        
+    }
+})
+
+
+// GET Product Offers 
+
+app.get('/products/:productId/offers', async (req, res) => {
+    const {productId} = req.params;
+    const { api_key } = req.query;
+
+  
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        res.json(JSON.parse(response))
+    } catch (error) {
+        
+    }
+})
+
+
+// GET Search
+
+app.get('/search/:searchQuery', async (req, res) => {
+    const {searchQuery} = req.params;
+    const { api_key } = req.query;
+
+  
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+        res.json(JSON.parse(response))
+    } catch (error) {
+        
+    }
+})
+
+
+
+
+
+
+app.listen(PORT, () => console.log('Server running on port '+  PORT) )
